bb167522dcd2d2b5a22f2c279b6a0dd4ea0eed34
Will Holcomb <dys@dhappy.org>
2021-05-11 03:28:58 -0400

    addin' variable metadata & mintin' 🎏

---
+++ packages/hardhat/contracts/WearablesNFTs.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol
+import "openzeppelin-solidity/contracts/token/ERC1155/ERC1155.sol";
+
+contract WearablesNFTs is ERC1155 {
+  event SetPurpose(address sender, string purpose);
+
+  string public purpose = "Building Unstoppable Apps";
+  mapping (uint256 => string) private _uris;
+  uint256 _tokenCount = 0;
+
+  constructor() ERC1155("Single Metadata URI Is Not Used") public {
+  }
+
+  function setPurpose(string memory newPurpose) public {
+    purpose = newPurpose;
+    console.log(msg.sender,"set purpose to",purpose);
+    emit SetPurpose(msg.sender, purpose);
+  }
+
+  function mint(address recipient, uint256 amount, string memory metadata, bytes memory data) internal virtual {
+    _tokenCount += 1;
+    uint256 id = _tokenCount;
+    _mint(recipient, id, amount, data);
+    setURI(metadata, id);
+  }
+
+  function uri(uint256 tokenId) public view virtual override returns (string memory) {
+    return _uris[tokenId];
+  }
+
+  function setURI(string memory newuri, uint256 tokenId) internal virtual {
+    // require something to prevent unauthorized access?
+    _uris[tokenId] = newuri;
+    emit URI(newuri, tokenId);
+  }
+}
