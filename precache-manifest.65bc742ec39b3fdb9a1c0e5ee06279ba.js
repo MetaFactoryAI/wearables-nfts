41124faf9d20e0495503aa373d062ac904183965
Will Holcomb <dys@dhappy.org>
2021-05-16 10:02:29 -0400

    Updates

---
+++ precache-manifest.65bc742ec39b3fdb9a1c0e5ee06279ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad78a5ef862dc4881692a845f9a0ffdf",
+    "url": "/nft-wearable/index.html"
+  },
+  {
+    "revision": "a6c6325b0aa84a416afa",
+    "url": "/nft-wearable/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "d5812826d227c9f401cd",
+    "url": "/nft-wearable/static/css/main.3f37dc0e.chunk.css"
+  },
+  {
+    "revision": "a6c6325b0aa84a416afa",
+    "url": "/nft-wearable/static/js/2.bc38b13b.chunk.js"
+  },
+  {
+    "revision": "53d78e7234fe6cf58dc23125b52ff5c1",
+    "url": "/nft-wearable/static/js/2.bc38b13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5812826d227c9f401cd",
+    "url": "/nft-wearable/static/js/main.ed9d6600.chunk.js"
+  },
+  {
+    "revision": "f71fcd6a6beb1d57cebd",
+    "url": "/nft-wearable/static/js/runtime-main.a3435d1d.js"
+  }
+]);
